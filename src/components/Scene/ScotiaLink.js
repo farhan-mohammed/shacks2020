b84ce4da8f2f164f0861a0a5181e6ec7caee65bd
Farhan Mohammed <farhanhm12@gmail.com>
2020-08-14 00:16:36 -0400

    Update ScotiaLink.js

--- src/components/Scene/ScotiaLink.js
+++ src/components/Scene/ScotiaLink.js
@@ -4,7 +4,7 @@ export default class ScotiaLink extends Component {
 	render() {
 		return (
 			<a className="sl" href={this.props.link}>
-				<img src={require('../../media/scotia.png')} />
+				<img src={require('../../media/scotia.png')} alt="" />
 				<div>{this.props.text}</div>
 			</a>
 		);
